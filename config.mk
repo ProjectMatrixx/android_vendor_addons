b457624d66b1a363fca89f6694fff3695a28b716
spezi77 <spezi7713@gmx.net>
2022-05-29 18:02:53 +0800

    addons: Add san francisco display font
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -166,6 +166,7 @@ PRODUCT_PACKAGES += \
     FontReemKufiOverlay \
     FontRobotoCondensedOverlay \
     FontRubikOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
     FontSamsungOneOverlay \
     FontSansSerifOverlay \
     FontSonySketchOverlay \
