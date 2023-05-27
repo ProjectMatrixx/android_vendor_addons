86e384b2efdd25aec345ac18f54d067e389d208b
drkphnx <dark.phnx12@gmail.com>
2023-05-27 19:10:57 +0530

    Addon: Add Basic Call Recorder

--- config.mk
+++ config.mk
@@ -374,6 +374,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     BetterQS
 
+# Basic Call Recorder
+PRODUCT_PACKAGES += \
+    Bcr
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
