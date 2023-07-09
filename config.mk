6a9ab6e9db816a8383e5d6fc709635d2eca113c2
mukesh22584 <mks22584@gmail.com>
2023-07-09 21:12:16 +0530

    prebuilt: Add Auxio & Via Browser

--- config.mk
+++ config.mk
@@ -258,6 +258,11 @@ PRODUCT_PACKAGES += \
     NavbarSammyOverlay \
     NavbarTecnoCamonOverlay
 
+# Prebuilt packages
+PRODUCT_PACKAGES += \
+    Auxio \
+    ViaBrowser
+    
 # Signal Icons
 PRODUCT_PACKAGES += \
     AquariumSignalOverlay \
