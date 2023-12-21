9656c64d82d485026ee051e84763fc69114051e6
mukesh22584 <mks22584@gmail.com>
2023-12-21 18:45:55 +0530

    addons: Add EvolveSans font - Also add missing GoogleSans overlay

--- config.mk
+++ config.mk
@@ -137,7 +137,9 @@ PRODUCT_PACKAGES += \
     FontCookieRunOverlay \
     FontCoolstoryOverlay \
     FontExotwoOverlay \
+    FontEvolveSansOverlay \
     FontFifa2018Overlay \
+    FontGoogleSansOverlay \
     FontGrandHotelOverlay \
     FontHarmonySansOverlay \
     FontLatoOverlay \
