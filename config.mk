83138258d5dbb1e63d5c350444ff9bb9a33c3981
ElDainosor <eldainosor@gmail.com>
2024-01-27 02:02:12 +0530

    UiStyles:  add dummy overlays for some themes
    - we don't need changes in these themes but because the overlays aren't completely disabled, we have to rely on this shitty hack

--- config.mk
+++ config.mk
@@ -350,6 +350,9 @@ PRODUCT_PACKAGES += \
 
 # UI styles
 PRODUCT_PACKAGES += \
+    UnmonetThemeAndroidOverlay \
+    UnmonetThemeSettingsOverlay \
+    UnmonetThemeSystemUIOverlay \
     AndroidEspressoThemeOverlay \
     AndroidSnowPaintDropThemeOverlay \
     AndroidVividThemeOverlay \
