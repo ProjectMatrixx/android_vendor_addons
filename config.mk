1a634b630b1309126ac5d127c6b41aa60111e720
Pranav Vashi <neobuddy89@gmail.com>
2024-07-27 11:25:35 +0530

    addons: Convert overlay to rro
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- config.mk
+++ config.mk
@@ -16,7 +16,6 @@ LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/addons/overlay/common
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/addons/overlay/common
 
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
