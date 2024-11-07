d8f2f275fc34ce0a67966332708ef8789b7d0bc0
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-07 17:09:50 +0800

    addons: Add NothingOS 3.0 lockscreen clock fonts
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -139,6 +139,8 @@ PRODUCT_PACKAGES += \
     ClockFontIOSOverlay \
     ClockFontHerculesOverlay \
     ClockFontSlimOverlay \
+    ClockFontNtype82Overlay \
+    ClockFontSubwayOverlay \
     FontAccuratistOverlay \
     FontAclonicaOverlay \
     FontAmaranteOverlay \
