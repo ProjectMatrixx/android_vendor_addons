cbdfec16bd4a6a14f6ce00bb54963b64f8216b26
darkphnx <dark.phnx12@gmail.com>
2025-02-10 18:14:59 +0530

    addons: add GeneralSans font overlay take from https://github.com/DroidX-UI/vendor_droidx/tree/15/fonts
    Co-authored-by: ZirgomHaidar <zirgomhaidar17@gmail.com>

--- config.mk
+++ config.mk
@@ -150,6 +150,7 @@ PRODUCT_PACKAGES += \
     FontGrandHotelOverlay \
     FontGoogleSansFlexOverlay \
     FontGoogleSansOverlay \
+    FontGeneralSansOverlay \
     FontHarmonySansOverlay \
     FontLatoOverlay \
     FontLinotteOverlay \
