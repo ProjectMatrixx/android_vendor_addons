182e546cc86e454a75d900f374d24e46ac1837c9
AnierinB <anierin@evolution-x.org>
2025-04-19 00:48:43 +0000

    extras: Add Navbar themes for Pixel Launcher
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config.mk
+++ config.mk
@@ -288,6 +288,20 @@ PRODUCT_PACKAGES += \
     IconShapeVesselOverlay
 
 # Navbar styles
+ifeq ($(WITH_GMS),true)
+PRODUCT_PACKAGES += \
+    NavbarAndroidPixelLauncherOverlay \
+    NavbarAsusPixelLauncherOverlay \
+    NavbarDoraPixelLauncherOverlay \
+    NavbarRisingPixelLauncherOverlay \
+    NavbarMotoPixelLauncherOverlay \
+    NavbarNexusPixelLauncherOverlay \
+    NavbarOldPixelLauncherOverlay \
+    NavbarOnePlusPixelLauncherOverlay \
+    NavbarOneUiPixelLauncherOverlay \
+    NavbarSammyPixelLauncherOverlay \
+    NavbarTecnoCamonPixelLauncherOverlay
+else
 PRODUCT_PACKAGES += \
     NavbarAndroidOverlay \
     NavbarAsusOverlay \
@@ -299,6 +313,7 @@ PRODUCT_PACKAGES += \
     NavbarOneUiOverlay \
     NavbarSammyOverlay \
     NavbarTecnoCamonOverlay
+endif
 
 # Power Menu Themes
 PRODUCT_PACKAGES += \
